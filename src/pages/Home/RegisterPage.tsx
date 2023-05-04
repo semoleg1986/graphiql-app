5e863d0e59d782868a470a8fe05f49d8ccd38072
Nikolai <nikolai.ivanov3110@gmail.com>
2023-05-04 19:42:12 +0400

    feat: update RegisterPage

--- src/pages/Home/RegisterPage.tsx
+++ src/pages/Home/RegisterPage.tsx
@@ -1,7 +1,16 @@
-import React from 'react';
+import { SignUp } from 'components/SignUp';
+import { Link } from 'react-router-dom';
 
 const RegisterPage = () => {
-  return <div></div>;
+  return (
+    <div>
+      <h1>Register</h1>
+      <SignUp />
+      <p>
+        Already have an account? <Link to="/login">Sign in</Link>
+      </p>
+    </div>
+  );
 };
 
 export default RegisterPage;
