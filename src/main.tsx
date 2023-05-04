30ef7ef6085c395c7c120ac529004dc7200ecd9f
Oleg Semenov <semoleg1986@gmail.com>
2023-05-04 18:26:56 +0300

    feat : add Firebase

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import { Provider } from 'react-redux';
 import App from './App';
 import './index.css';
 import { store } from './store';
+import './firebase';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
