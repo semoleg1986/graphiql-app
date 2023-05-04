bb0bbfedc60e49eca642bde978a2160a9327c1aa
Nikolai <nikolai.ivanov3110@gmail.com>
2023-05-04 19:41:59 +0400

    feat: update LoginPage

--- src/pages/Home/LoginPage.tsx
+++ src/pages/Home/LoginPage.tsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Login } from '../../components/Login';
 
 const LoginPage = () => {
-  return <div></div>;
+  return (
+    <div>
+      <h1>Login</h1>
+      <Login />
+      <p>
+        Or <Link to="/register">register</Link>
+      </p>
+    </div>
+  );
 };
 
 export default LoginPage;
