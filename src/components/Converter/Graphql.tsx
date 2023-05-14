0dd1ab0f5c1b9a9085edc5ff12c6cc540e1579ed
Oleg Semenov <semoleg1986@gmail.com>
2023-05-14 16:00:13 +0300

    feat: add graphiql

--- src/components/Converter/Graphql.tsx
+++ src/components/Converter/Graphql.tsx
@@ -1,21 +1,37 @@
 import React, { useState } from 'react';
+import ApiService from '../../services/ApiServ';
 
 const GraphiQL: React.FC = () => {
   const [query, setQuery] = useState('');
+  const [result, setResult] = useState('');
 
   const handleQueryChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
     setQuery(event.target.value);
   };
 
-  const executeQuery = () => {
-    // Здесь можно выполнить запрос к вашему серверу GraphQL с использованием выбранного клиента или библиотеки, например, Apollo Client или Axios.
-    console.log('Executing query:', query);
+  const executeQuery = async () => {
+    try {
+      // Здесь мы используем axios для выполнения запроса к серверу GraphQL
+      const response = await ApiService.post('tv/popular', { query });
+
+      // Получаем результат из ответа сервера
+      const data = response.data;
+
+      // Обновляем состояние с результатом запроса
+      setResult(JSON.stringify(data, null, 2));
+    } catch (error) {
+      console.error('Ошибка при выполнении запроса:', error);
+    }
   };
 
   return (
     <div>
       <textarea value={query} onChange={handleQueryChange} rows={10} cols={50} />
       <button onClick={executeQuery}>Выполнить запрос</button>
+      <div>
+        <h2>Результат:</h2>
+        <pre>{result}</pre>
+      </div>
     </div>
   );
 };
