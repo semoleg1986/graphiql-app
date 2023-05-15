b0182167c76c7db785b980e9df3b9cb650e39cb6
Nikolai <nikolai.ivanov3110@gmail.com>
2023-05-15 12:48:15 +0400

    feat: add buttons to welcomepage

--- src/pages/Home/WelcomePage.tsx
+++ src/pages/Home/WelcomePage.tsx
@@ -1,7 +1,28 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { useAuth } from '../../hooks/use-auth';
+import './WelcomePage.css';
 
 const WelcomePage = () => {
-  return <div>Welcome</div>;
+  const { isAuth } = useAuth();
+
+  return isAuth ? (
+    <div>
+      Welcome
+      <Link to="/home">
+        <button className="gomainpage">Go to main</button>
+      </Link>
+    </div>
+  ) : (
+    <div className="button-container">
+      <Link to="/login">
+        <button>Sign In</button>
+      </Link>
+      <Link to="/register">
+        <button>Sign Up</button>
+      </Link>
+    </div>
+  );
 };
 
 export default WelcomePage;
