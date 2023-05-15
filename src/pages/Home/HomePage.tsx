ba8d3b83d79cb8fb1b251c155e0d7405cfe98d3a
Nikolai <nikolai.ivanov3110@gmail.com>
2023-05-15 16:56:07 +0400

    feat: add redirect to welcomepage

--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -1,25 +1,25 @@
 import { Navigate } from 'react-router-dom';
 import { useAuth } from '../../hooks/use-auth';
-import { removeUser } from '../../store/slices/userSlice';
-import { useAppDispatch } from '../../hooks/redux-hooks';
+// import { removeUser } from '../../store/slices/userSlice';
+// import { useAppDispatch } from '../../hooks/redux-hooks';
 import i18next from 'i18next';
 import GraphiQL from '../../components/Converter/Graphql';
 
 const HomePage = () => {
-  const dispatch = useAppDispatch();
+  // const dispatch = useAppDispatch();
 
-  const { isAuth, email } = useAuth();
+  const { isAuth } = useAuth();
 
   const title = i18next.t('homepage.title');
-  const logout = i18next.t('homepage.logout');
+  // const logout = i18next.t('homepage.logout');
 
   return isAuth ? (
     <div>
       <h1>{title}</h1>
 
-      <button onClick={() => dispatch(removeUser())}>
+      {/* <button onClick={() => dispatch(removeUser())}>
         {logout} {email}
-      </button>
+      </button> */}
       <GraphiQL />
     </div>
   ) : (
