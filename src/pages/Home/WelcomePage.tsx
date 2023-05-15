1bef3636eea6b3de0911d255e2811168ea358535
Nikolai <nikolai.ivanov3110@gmail.com>
2023-05-15 16:52:04 +0400

    refactor: change buttons on welcomepage

--- src/pages/Home/WelcomePage.tsx
+++ src/pages/Home/WelcomePage.tsx
@@ -1,22 +1,35 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { useAuth } from '../../hooks/use-auth';
-import './WelcomePage.css';
+import './Buttons.css';
 
 const WelcomePage = () => {
   const { isAuth } = useAuth();
 
-  return isAuth ? (
+  return (
     <div>
-      Welcome
-      <Link to="/">
-        <button className="gomainpage">Go to main</button>
-      </Link>
-    </div>
-  ) : (
-    <div className="button-container">
-      <Link to="/login">Sign In</Link>
-      <Link to="/register">Sign Up</Link>
+      <h1>Welcome</h1>
+      <p>Some additional text fot all users</p>
+      <p>
+        The welcome page should contain general information about the developers, project, and
+        course
+      </p>
+      {isAuth ? (
+        <div className="button-container">
+          <Link to="/">
+            <button className="gomainpage">Go to Main page</button>
+          </Link>
+        </div>
+      ) : (
+        <div className="button-container">
+          <Link to="/login">
+            <button className="signin">Sign In</button>
+          </Link>
+          <Link to="/register">
+            <button className="signup">Sign Up</button>
+          </Link>
+        </div>
+      )}
     </div>
   );
 };
