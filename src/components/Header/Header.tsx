e2f0455b5ca290155a9b2b3407b74d26a09a6230
Nikolai <nikolai.ivanov3110@gmail.com>
2023-05-18 16:01:41 +0400

    fix: update signout

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,12 @@
-import { useState, useEffect } from 'react';
+// import { useState, useEffect } from 'react';
 import { getAuth, signOut } from 'firebase/auth';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useNavigate } from 'react-router-dom';
 import { useAuth } from '../../hooks/use-auth';
 import { NavLink } from 'react-router-dom';
 import i18next from 'i18next';
 
 const Header = () => {
+  const navigate = useNavigate();
   const { isAuth } = useAuth();
   const auth = getAuth(); // Получение объекта аутентификации
 
@@ -14,6 +15,7 @@ const Header = () => {
       .then(() => {
         // Выход из системы успешно выполнен
         console.log('Выход из системы выполнен');
+        navigate('/');
       })
       .catch((error) => {
         // Произошла ошибка при выходе из системы
@@ -21,7 +23,7 @@ const Header = () => {
       });
   };
 
-  return isAuth ? (
+  return (
     <header>
       <h1>
         <Routes>
@@ -32,23 +34,13 @@ const Header = () => {
       </h1>
       <NavLink to="/main">{i18next.t('main')}</NavLink>
       <NavLink to="/about">{i18next.t('about')}</NavLink>
-      <div className="button-container">
-        <button className="signout" onClick={handleSignOut}>
-          {i18next.t('signout')}
-        </button>
-      </div>
-    </header>
-  ) : (
-    <header>
-      <h1>
-        <Routes>
-          <Route path="/main" element={i18next.t('main')} />
-          <Route path="/about" element={i18next.t('about')} />
-          <Route path="" element={i18next.t('notfound')} />
-        </Routes>
-      </h1>
-      <NavLink to="/main">{i18next.t('main')}</NavLink>
-      <NavLink to="/about">{i18next.t('about')}</NavLink>
+      {isAuth && (
+        <div className="button-container">
+          <button className="signout" onClick={handleSignOut}>
+            {i18next.t('signout')}
+          </button>
+        </div>
+      )}
     </header>
   );
 };
