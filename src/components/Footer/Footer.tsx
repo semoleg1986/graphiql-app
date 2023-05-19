de700f1a2a988e1c3af989e487aa5873ad69ffdf
Nikolai <nikolai.ivanov3110@gmail.com>
2023-05-20 00:10:51 +0400

    fix: update footer

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div>
+        <div className="footer-content">
+          <div className="footer-links">
+            <span>Authors: </span>
+            <a href="https://github.com/semoleg1986">semoleg1986</a>
+            <span> | </span>
+            <a href="https://github.com/NickIvn">NickIvn</a>
+          </div>
+          <div className="footer-logo">
+            <a href="https://rs.school/react/">
+              <img src="https://rs.school/images/rs_school_js.svg" alt="Course Logo" />
+            </a>
+          </div>
+          <div className="footer-year">&copy; 2023 GraphiQL. Final Task. All rights reserved. </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
