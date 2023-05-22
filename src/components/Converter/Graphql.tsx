248014b00eb15071856e4b443afba134cc4f6438
Oleg Semenov <semoleg1986@gmail.com>
2023-05-23 01:44:07 +0300

    feat: add variables

--- src/components/Converter/Graphql.tsx
+++ src/components/Converter/Graphql.tsx
@@ -12,6 +12,8 @@ const GraphiQL = () => {
   const [query, setQuery] = useState('');
   const [result, setResult] = useState('');
   const [showDocs, setShowDocs] = useState(false);
+  const [variables, setVariables] = useState('{}');
+  const [headers, setHeaders] = useState('');
 
   const handleClick = () => {
     setShowDocs(true);
@@ -20,6 +22,13 @@ const GraphiQL = () => {
   const handleClose = () => {
     setShowDocs(false);
   };
+  const onChangeVariables = React.useCallback((value: string) => {
+    setVariables(value);
+  }, []);
+
+  const onChangeHeaders = React.useCallback((value: string) => {
+    setHeaders(value);
+  }, []);
 
   useEffect(() => {
     const fetchSchema = async () => {
@@ -50,11 +59,18 @@ const GraphiQL = () => {
   }, []);
   const executeQuery = async () => {
     try {
+      const variablesObj = variables ? JSON.parse(variables) : {}; // Преобразование переменных в объект JSON
+      const headersObj = headers ? JSON.parse(headers) : {}; // Преобразование заголовков в объект JSON
+
       const response = await fetch(endpoint, {
         method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ query }),
+        headers: {
+          'Content-Type': 'application/json',
+          ...headersObj, // Применение заголовков
+        },
+        body: JSON.stringify({ query, variables: variablesObj }), // Применение переменных
       });
+
       const result = await response.json();
       setResult(JSON.stringify(result, null, 2));
     } catch (err) {
@@ -68,32 +84,54 @@ const GraphiQL = () => {
 
   return (
     <div className="query-container">
-      {showDocs ? (
-        <div>
-          <button onClick={handleClose}>x</button>
-          <iframe
-            style={{ width: '100%', height: '600px' }}
-            src="/docs/index.html"
-            title="GraphQL documentation"
-          ></iframe>
-        </div>
-      ) : (
-        <button className="docs-button" onClick={handleClick}>
-          Docs
+      <div className="column">
+        {showDocs ? (
+          <div>
+            <button onClick={handleClose}>x</button>
+            <iframe
+              style={{ width: '100%', height: '600px' }}
+              src="/docs/index.html"
+              title="GraphQL documentation"
+            ></iframe>
+          </div>
+        ) : (
+          <button className="docs-button" onClick={handleClick}>
+            Docs
+          </button>
+        )}
+      </div>
+      <div className="column">
+        <CodeMirror
+          className="query-editor"
+          value={query}
+          height="500px"
+          width="100%"
+          extensions={[graphql(schema)]}
+          onChange={onChangeValue}
+        />
+        <button className="run-button" onClick={executeQuery}>
+          Run
         </button>
-      )}
-      <CodeMirror
-        className="query-editor"
-        value={query}
-        height="500px"
-        width="100%"
-        extensions={[graphql(schema)]}
-        onChange={onChangeValue}
-      />
-      <button className="run-button" onClick={executeQuery}>
-        Run
-      </button>
-      <CodeMirror className="response-field" value={result} height="500px" width="100%" />
+        <h3>Variables</h3>
+        <CodeMirror
+          className="variable-editor"
+          value={variables}
+          height="100px"
+          width="100%"
+          onChange={onChangeVariables}
+        />
+        <h3>Headers</h3>
+        <CodeMirror
+          className="headers-editor"
+          value={headers}
+          height="100px"
+          width="100%"
+          onChange={onChangeHeaders}
+        />
+      </div>
+      <div className="column">
+        <CodeMirror className="response-field" value={result} height="500px" width="100%" />
+      </div>
     </div>
   );
 };
