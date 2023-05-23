db3ac4bcfb6fb6196b40923d8f0906398a2189cb
Oleg Semenov <semoleg1986@gmail.com>
2023-05-24 02:11:42 +0300

    feat: add img

--- src/pages/404/NotFoundPage.tsx
+++ src/pages/404/NotFoundPage.tsx
@@ -8,6 +8,10 @@ const NotFoundPage = () => {
       <Link to="/">
         <button className="signin">{i18next.t('backtopage')}</button>
       </Link>
+      <img
+        src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/22533fb0-3db2-4d45-8675-4aaf09a2027b/dcg7gx1-145cab41-ec59-4ba1-808d-b3d9532409e9.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzIyNTMzZmIwLTNkYjItNGQ0NS04Njc1LTRhYWYwOWEyMDI3YlwvZGNnN2d4MS0xNDVjYWI0MS1lYzU5LTRiYTEtODA4ZC1iM2Q5NTMyNDA5ZTkucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.y-EzqWwAP55JiAx-s_DES2b4IscaaO1goPcnkA_OkFo"
+        alt=""
+      />
     </>
   );
 };
--- src/pages/Main/WelcomePage.tsx
+++ src/pages/Main/WelcomePage.tsx
@@ -12,6 +12,10 @@ const WelcomePage = () => {
       <h1>{i18next.t('welcome')}</h1>
       <h3>{i18next.t('welcome_text1')}</h3>
       <p>{i18next.t('welcome_text2')}</p>
+      <div className="yoda-1">
+        <img src="https://www.pngmart.com/files/3/Star-Wars-PNG-Clipart.png" alt="" />
+      </div>
+
       {isAuth ? (
         <div className="button-container">
           <Link to="/main">
