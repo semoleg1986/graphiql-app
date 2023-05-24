b2741e9522c6327976b4aa1935fca8d534659e66
Nikolai <nikolai.ivanov3110@gmail.com>
2023-05-24 13:11:34 +0400

    feat: update about page
    add text and localozation

--- src/pages/About/AboutPage.tsx
+++ src/pages/About/AboutPage.tsx
@@ -2,9 +2,12 @@ import i18next from 'i18next';
 
 const About = () => {
   return (
-    <div>
+    <div className="about">
       <h1>{i18next.t('about')}</h1>
-      <p>{i18next.t('about_text')}</p>
+      <p>{i18next.t('about_para')}</p>
+      <p>{i18next.t('about_para1')}</p>
+      <p>{i18next.t('about_para2')}</p>
+      <p>{i18next.t('about_para3')}</p>
     </div>
   );
 };
