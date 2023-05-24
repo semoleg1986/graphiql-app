6a1a19f2f545474f0aa19841f8eda0755da7e956
Nikolai <nikolai.ivanov3110@gmail.com>
2023-05-24 16:57:58 +0400

    fix: update about page
    update about content, update localization files

--- src/pages/About/AboutPage.tsx
+++ src/pages/About/AboutPage.tsx
@@ -8,6 +8,12 @@ const About = () => {
       <p>{i18next.t('about_para1')}</p>
       <p>{i18next.t('about_para2')}</p>
       <p>{i18next.t('about_para3')}</p>
+      <p>
+        {i18next.t('about_para4')}
+        <a href="https://develop--golden-clafoutis-688ad6.netlify.app/">netlify</a>
+        {i18next.t('about_para5')}
+        <a href="https://github.com/semoleg1986/graphiql-app">GitHub.</a>
+      </p>
     </div>
   );
 };
