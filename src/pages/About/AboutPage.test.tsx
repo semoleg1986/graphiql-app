18b5c62bb230c5c64bcc60f516054cb0052671db
Nikolai <nikolai.ivanov3110@gmail.com>
2023-05-28 21:56:53 +0400

    refactor: delete unused files

--- src/pages/About/AboutPage.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import About from './AboutPage';
-
-describe('renders learn react link', () => {
-  test('render App', () => {
-    render(<About />);
-    expect(screen.getByText(/About/i)).toBeInTheDocument();
-  });
-});
